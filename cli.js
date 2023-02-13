fc503318940cc91217b402c88c3416f5c7689d5e
RishiM0 <rishi.mantri0803@gmail.com>
2023-02-13 17:06:21 -0500

    all response replaced with data

--- cli.js
+++ cli.js
@@ -52,7 +52,7 @@ if (args.d){
 }
 
 let need_galoshes = '';
-if (response.daily_precipitation_hours[days] > 1){
+if (data.daily.precipitation_hours[days] > 1){
     need_galoghes = data.daily.precipitation_hours[days] + 'is expected, you will need galoshes ';
 } else {
     need_galoshes = data.daily.precipitation_hours[days] + 'is expected, you will not need galoshes ';
