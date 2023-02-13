64f1666c1b7e79a9e9795c92f68a6fc70f350a10
RishiM0 <rishi.mantri0803@gmail.com>
2023-02-13 17:35:28 -0500

    url change

--- cli.js
+++ cli.js
@@ -43,7 +43,7 @@ if (args.e) {
     longitude = -(args.w);
 }
 
-const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=' + latitude + '&longitude=' + longitude + '&daily=precipitation_hours&current_weather=true'+ '&timezone=' + timezone);
+const response = await fetch("https://api.open-meteo.com/v1/forecast?latitude=" + latitude + "&longitude=" + longitude + "&timezone=" + timezone + "&daily=precipitation_hours");
 const data = await response.json();
 
 let days = args.d;
