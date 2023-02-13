d60fbf950b01ff932079628fecf5993b8a8fbb4a
RishiM0 <rishi.mantri0803@gmail.com>
2023-02-13 17:04:20 -0500

    data

--- cli.js
+++ cli.js
@@ -53,9 +53,9 @@ if (args.d){
 
 let need_galoshes = '';
 if (response.daily_precipitation_hours[days] > 1){
-    need_galoghes = response.daily_precipitation_hours[days] + 'is expected, you will need galoshes ';
+    need_galoghes = data.daily.precipitation_hours[days] + 'is expected, you will need galoshes ';
 } else {
-    need_galoshes = response.daily_precipitation_hours[days] + 'is expected, you will not need galoshes ';
+    need_galoshes = data.daily.precipitation_hours[days] + 'is expected, you will not need galoshes ';
 }
 
 if (days == 0) {
